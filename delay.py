5a8e56f213a99a996b334420cdd54976fc5b3588
fruitsnack1 <tomas.ryzner@email.cz>
2022-08-04 13:14:48 +0200

    added timer

--- delay.py
+++ delay.py
@@ -1,92 +1,116 @@
 from datetime import datetime
-import numpy as np
-import cv2
-from mss import mss
-from PIL import Image
-from termcolor import colored
-import numpy as np
-from colorama import Fore, Back, Style
 import os
+import re
+import signal
+import sys
+import threading
+from time import sleep
+from PIL import ImageEnhance
+from pytesseract import pytesseract
 import pyautogui
+import tkinter as tk
+
+HEIGHT = 300
+WIDTH = 650
+START_TIME = datetime.now().timestamp()
+LOG_NAME = f'log{datetime.fromtimestamp(START_TIME)}.log'
+SAVE_PREVIEW_IMG = True
 
 
-colors = [Back.GREEN, Back.RED, Back.BLUE]
-counter = 0
-mon = {'left': 0, 'top': 200, 'width': 200, 'height': 200}
-timestamp = datetime.now().timestamp()
-changeTimestamp = datetime.now().timestamp()
-currentDelay = 0
-calculated = False
-cameraColor = ''
+window = tk.Tk()
+window.title('')
+window.resizable(width=False, height=False)
+
+var = tk.StringVar()
+var.set(str(datetime.now().timestamp()))
+
+label = tk.Label(window, textvariable=var,
+                 font=('Monospace', 40), padx=5, pady=5, background='white')
+label.pack()
+quitting = False
 
 
-def printSquare(col: str) -> None:
-    for a in range(5):
-        print(col + f"{currentDelay} {cameraColor}\n")
-    print(Style.RESET_ALL)
-    # os.system('clear')
+def takeScreenshot():
+    screen = pyautogui.screenshot(
+        region=(1920 * 2 - WIDTH, 1200 - HEIGHT, WIDTH, HEIGHT))
+    if SAVE_PREVIEW_IMG:
+        screen.save('./screenshot.png')
+    return screen
 
 
-def changeColor():
-    global counter
-    global timestamp
-    global changeTimestamp, calculated
-    changeTimestamp = datetime.now().timestamp()
-    timestamp = curr
-    counter = (counter + 1) % len(colors)
-    calculated = True
-    # printSquare(colors[counter])
+def calculateDelay(img) -> float:
+    text = getImageText(img)
+    # filter characters
+    result = re.sub(r'[^\d.]', '', text)
+    print(result)
+    if len(result) != 34:
+        return -1
+    return float(result[17:34]) - float(result[0:17])
 
 
-with mss() as sct:
+def getImageText(img) -> str:
+    img = img.convert('L')
+    text = pytesseract.image_to_string(img)
+    return text
+
+
+def addSummary() -> None:
+    seconds = round(datetime.now().timestamp() - START_TIME, 0)
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    try:
+        f = open(f'./logs/{LOG_NAME}', 'r')
+        lines = f.readlines()
+        delays = []
+        for line in lines:
+            delays.append(float(line[34:52]))
+        f.close()
+        f = open(f'./logs/{LOG_NAME}', 'a')
+        f.write('\nSUMMARY:\n')
+        f.write(f'Test time: {int(h)}:{int(m)}:{int(s)}\n')
+        f.write(f'Min delay: {min(delays)} s\n')
+        f.write(f'Avg delay: {avg(delays)} s\n')
+        f.write(f'Max delay: {max(delays)} s\n')
+        f.close()
+    except Exception:
+        pass
+
+
+def avg(array: list) -> float:
+    return sum(array) / len(array)
+
+
+def quit(signal, frame) -> None:
+    global quitting
+    addSummary()
+    quitting = True
+    sys.exit(0)
+
+
+def updateWindow():
+    var.set('{:.6f}'.format(round(datetime.now().timestamp(), 6)))
+    window.update_idletasks()
+
+
+signal.signal(signal.SIGINT, quit)
+if not os.path.exists('./logs'):
+    os.makedirs('./logs')
+
+
+def doStuff():
+    while not quitting:
+        screenshot = takeScreenshot()
+        delay = calculateDelay(screenshot)
+        if delay > 0 and delay < 15:
+            print(f'current delay :{delay}')
+            f = open(f'./logs/{LOG_NAME}', 'a')
+            f.write(f'{datetime.now()} delay :{delay}\n')
+            f.close()
+        sleep(2)
+
+
+if __name__ == "__main__":
+    threading.Thread(target=doStuff).start()
     while True:
+        updateWindow()
         curr = datetime.now().timestamp()
-        if curr - timestamp > 3:
-            changeColor()
-
-        screenShot = sct.grab(mon)
-        img = Image.frombytes(
-            'RGB',
-            (screenShot.width, screenShot.height),
-            screenShot.rgb,
-        )
-        cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
-        cv2.imshow('test', np.array(img))
-
-        # setting values for base colors
-        b = np.array(img)[:, :, :1]
-        g = np.array(img)[:, :, 1:2]
-        r = np.array(img)[:, :, 2:]
-
-        # computing the mean
-        b_mean = np.mean(b)
-        g_mean = np.mean(g)
-        r_mean = np.mean(r)
-
-        print(r_mean)
-        print(g_mean)
-        print(b_mean)
-
-        color = ''
-        # displaying the most prominent color
-        if ((b_mean > g_mean) and (b_mean > r_mean)):
-            color = Back.BLUE
-            cameraColor = 'blue'
-            print('red')
-        if ((g_mean > r_mean) and (g_mean > b_mean)):
-            color = Back.GREEN
-            cameraColor = 'green'
-            print('green')
-        if ((r_mean > b_mean) and (r_mean > g_mean)):
-            color = Back.RED
-            cameraColor = 'red'
-            print('blue')
-
-        if color == colors[counter] and not calculated:
-            currentDelay = datetime.now().timestamp() - changeTimestamp
-            calculated = True
-        if cv2.waitKey(33) & 0xFF in (
-            ord('q'),
-            27,
-        ):
-            break
